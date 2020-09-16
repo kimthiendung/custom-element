7bc38234f82a29592348c3d032c864eab2702eec
kimthiendung90 <kimthiendung@gmail.com>
2020-09-16 21:48:27 +0700

    new table

---
+++ src/components/Table/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr>
+          <cell><app-element myprop="ddd"></app-element></cell>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import AppElement from "../../App"
+import Cell from "./Cell"
+export default {
+  name: "io-table",
+  components: { Cell, AppElement },
+  props: ["data"]
+}
+</script>
---
+++ src/components/Table/index.js
@@ -0,0 +1,3 @@
+import "./style.scss";
+import Main from "./Main";
+export default Main;
